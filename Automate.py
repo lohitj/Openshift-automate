6c6f781f2921617be3abb8dd397aba53a63d9928
lohitj <39816251+lohitj@users.noreply.github.com>
2019-01-24 22:34:17 +0530

    Update Automate.py

--- Automate.py
+++ Automate.py
@@ -1,2 +1,3 @@
+#!/usr/local/bin/python
 print("wget http://jenkins-cicd-test.apps.na39.openshift.opentlc.com/jnlpJars/jenkins-cli.jar")
 print("java -jar jenkins-cli.jar -s https://jenkins-coolstore-cicd-lohit.apps.na39.openshift.opentlc.com/ create-job cart-service < Template.xml")
