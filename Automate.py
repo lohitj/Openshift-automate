86d5cea32ea39fa7b0b77cacd9b62e98984a46d0
lohitj <39816251+lohitj@users.noreply.github.com>
2019-01-24 22:31:51 +0530

    Update and rename Automate.sh to Automate.py

---
+++ Automate.py
@@ -0,0 +1,2 @@
+print("wget http://jenkins-cicd-test.apps.na39.openshift.opentlc.com/jnlpJars/jenkins-cli.jar")
+print("java -jar jenkins-cli.jar -s https://jenkins-coolstore-cicd-lohit.apps.na39.openshift.opentlc.com/ create-job cart-service < Template.xml")
--- Automate.sh
+++
@@ -1,2 +0,0 @@
-wget http://jenkins-cicd-test.apps.na39.openshift.opentlc.com/jnlpJars/jenkins-cli.jar
-java -jar jenkins-cli.jar -s https://jenkins-coolstore-cicd-lohit.apps.na39.openshift.opentlc.com/ create-job cart-service < Template.xml
