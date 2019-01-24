fe45049676b8163cc20ad0e7fb9657de6c4eed9a
lohitj <39816251+lohitj@users.noreply.github.com>
2019-01-24 23:27:44 +0530

    Update Automate.py

--- Automate.py
+++ Automate.py
@@ -4,3 +4,4 @@ os.system('ls')
 services = ['cart-service', 'catalog-service','coolstore-gw','inventory-service','pricing-service','rating-service','review-service','sso-service'];
 for service in services:
 	os.system("java -jar jenkins-cli.jar -s https://jenkins-coolstore-cicd-lohit.apps.na39.openshift.opentlc.com/ create-job "+service+" < Template.xml")
+	os.system("java -jar jenkins-cli.jar -s https://jenkins-coolstore-cicd-lohit.apps.na39.openshift.opentlc.com/ build "+service)
