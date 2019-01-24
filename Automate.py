7b61cec0dda1fd457f72136bdb0867fc3da2cb90
lohitj <39816251+lohitj@users.noreply.github.com>
2019-01-24 22:38:03 +0530

    Update Automate.py

--- Automate.py
+++ Automate.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python
 import os
 os.system('ls')
-//os.system('wget http://jenkins-cicd-test.apps.na39.openshift.opentlc.com/jnlpJars/jenkins-cli.jar')
+
 os.system('java -jar jenkins-cli.jar -s https://jenkins-coolstore-cicd-lohit.apps.na39.openshift.opentlc.com/ create-job cart-service < Template.xml')
