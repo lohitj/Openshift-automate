2ba5d11c0772b2cf029b62bd3231538642539f10
lohitj <39816251+lohitj@users.noreply.github.com>
2019-01-25 10:25:56 +0530

    Update OpenshiftOC.py

--- OpenshiftOC.py
+++ OpenshiftOC.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python
 import os
 os.system('ls')
-services = ['cart-service', 'catalog-service','coolstore-gw','inventory-service','pricing-service','rating-service','review-service','sso-service'];
+services = ['cart-service', 'catalog-service','coolstore-gw'];
 os.system("oc login https://master.na39.openshift.opentlc.com --token=gA8ocw_icOIsFybOUvinkoy0EIaaTouuOu6AxuExLp8")
 for service in services:
   os.system("oc new-app https://github.com/akilans/ms-node-app.git --strategy=pipeline --name="+service+"-pipeline")
