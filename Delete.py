a1d8a114429cb97603cc5d50dd08cafeffaf1c14
lohitj <39816251+lohitj@users.noreply.github.com>
2019-01-25 10:33:29 +0530

    Update Delete.py

--- Delete.py
+++ Delete.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 import os
 os.system('ls')
-services = ['cart-service', 'catalog-service','coolstore-gw','inventory-service','pricing-service','rating-service','review-service','sso-service'];
+services = ['cart-service', 'catalog-service','coolstore-gw'];
 for service in services:
 	os.system("java -jar jenkins-cli.jar -s https://jenkins-coolstore-cicd-lohit.apps.na39.openshift.opentlc.com/ delete-job "+service)
